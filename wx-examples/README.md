6c985652794cbc99bd2777757b65fdaf12dab89e
page <hanmingpan@igengmei.com>
2018-09-29 13:32:37 +0800

    wx-examples get

---
+++ wx-examples/README.md
@@ -0,0 +1,6 @@
+# mutiple-axios wx-examples
+
+To run the examples:
+1. `git clone git@github.com:hanmingpan/multiple-axios.git`
+2. 下载`微信开发者工具`
+3. 新建小程序项目
---
+++ wx-examples/get/index.js
@@ -0,0 +1,19 @@
+const axios = require('../lib/axios.js')
+const config = require('../config')
+
+Page({
+  data: {
+    people: []
+  },
+  onLoad() {
+    axios.get(config.host + '/get/server')
+      .then(res => {
+        this.setData({
+          people: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+})
